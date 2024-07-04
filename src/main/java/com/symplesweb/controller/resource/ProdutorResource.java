ed96a08ea24024cd9ff33af7b9f7d8792323fac3
Wanderson Lira <wandersonlira.dev1@gmail.com>
2024-07-04 13:49:58 -0300

    :sparkles: feat: Documentado camada resource class 'ProdutorResource.java' resolved #9

--- src/main/java/com/symplesweb/controller/resource/ProdutorResource.java
+++ src/main/java/com/symplesweb/controller/resource/ProdutorResource.java
@@ -24,16 +24,31 @@ import com.symplesweb.controller.resource.exception.InvalidUpdateException;
 import com.symplesweb.controller.services.ProdutorService;
 import com.symplesweb.model.entities.Produtor;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 
 @RestController
-@RequestMapping(value = "/produtores")
+@RequestMapping(value = "/produtores", produces = "application/json")
+@Tag(name = "Produtor", description = "Produtor é alguém responsável por criar, organizar, gerenciar e disponibiliza "
+		+ "eventos para o público.")
 public class ProdutorResource {
 	
 	@Autowired
 	private ProdutorService service;
 	
 	
-	
+	@Operation(summary = "Cadastra um novo produtor sem nenhum evento associado", method = "POST", description = "O cadastro deve ter um "
+			+ "CNPJ e E-mail válido caso contrário apresentará uma exeception, além disso, os campos não devem ser vazio.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "O status HTTP \"201 Created\" é utilizado como resposta de sucesso, indica que a "
+					+ "requisição foi bem sucedida e que um novo produtor foi criado. "),
+			@ApiResponse(responseCode = "400", description = "O código de status de resposta HTTP 400 Bad Request indica que o servidor não pode "
+					+ "ou não irá processar a requisição devido a alguma coisa que foi entendida como um erro do cliente (por exemplo, sintaxe de "
+					+ "requisição mal formada, algum campo vazio ou algum campo fora do padrão estabelecido).")
+	})
 	@PostMapping
 	public ResponseEntity<ProdutorDtoView> save(@RequestBody ProdutorDto produtorDto) {
 		Produtor produtorToSave = produtorDto.toEntity();
@@ -42,7 +57,12 @@ public class ProdutorResource {
 	}
 	
 	
-	
+	@Operation(summary = "Exibe todos os eventos uma vez criado", method = "GET", description = "Neste endpoint é exibido "
+			+ "todos os eventos cadastrados onde serão visualizados pelos participantes.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "A solicitação foi bem-sucedida"),
+			@ApiResponse(responseCode = "404", description = "O servidor não pode encontrar o recurso solicitado")
+	})
 	@GetMapping
 	public ResponseEntity<List<ProdutorDtoView>> findAll() {
 		List<ProdutorDtoView> listProdutor = this.service.findAll().stream().map(produtor -> new ProdutorDtoView(produtor))
@@ -51,6 +71,12 @@ public class ProdutorResource {
 	}
 	
 	
+	@Operation(summary = "Busca produtor baseado no parâmetro ID", method = "GET", description = "O retorno deste endpoint será uma uníca "
+			+ "tupla da tabela baseado no ID passado no parâmetro e, caso exista eventos associados, todos serão exibidos.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "A solicitação foi bem-sucedida"),
+			@ApiResponse(responseCode = "404", description = "O servidor não pode encontrar o recurso solicitado")
+	})
 	@GetMapping(value = "/{id_produtor}")
 	public ResponseEntity<ProdutorDtoView> findById(@PathVariable Long id_produtor) {
 		Produtor produtor = service.findById(id_produtor);
@@ -58,7 +84,15 @@ public class ProdutorResource {
 	}
 	
 	
-	
+	@Operation(summary = "Atualiza os dados pessoais do produtor", method = "PATH", description = "Será realizado uma busca baseado no ID "
+			+ "do produtor, caso o ID exista no banco e os dados informado no corpo do método seja diferente do armazenado no banco, acontecerá a atualização dos dados.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "O status HTTP \"201 Created\" é utilizado como resposta de sucesso, indica que a "
+					+ "requisição foi bem sucedida e que um novo recurso foi criado. "),
+			@ApiResponse(responseCode = "400", description = "O código de status de resposta HTTP 400 Bad Request indica que o servidor não pode "
+					+ "ou não irá processar a requisição devido a alguma coisa que foi entendida como um erro do cliente (por exemplo, sintaxe de "
+					+ "requisição mal formada, algum campo vazio ou algum campo fora do padrão estabelecido).")
+	})
 	@PatchMapping
 	public ResponseEntity<ProdutorDtoView> updateProdutor(
 			@RequestParam(value = "id_produtor") Long id_produtor,
@@ -79,7 +113,13 @@ public class ProdutorResource {
 	}
 	
 	
-	
+	@Operation(summary = "Exclui produtor com ID cadastrado", method = "DELETE", description = "A exclusão acontecerá nas seguintes "
+			+ "condições: 1° - ID deve existir no servidor, 2° - O produtor não deve ter eventos associado")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "404", description = "O servidor não pode encontrar o recurso solicitado"),
+			@ApiResponse(responseCode = "422", description = "A solicitação não pôde ser atendida devido a erros semânticos."),
+			@ApiResponse(responseCode = "500", description = "O servidor encontrou uma situação com a qual não sabe lidar.")
+	})
 	@DeleteMapping(value = "/{id_produtor}")
 	@ResponseStatus(value = HttpStatus.NO_CONTENT)
 	public void deleteById(@PathVariable Long id_produtor) {
