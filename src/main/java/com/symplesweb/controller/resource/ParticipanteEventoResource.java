97c369e6237c8f762ac8ec68985c561960de246e
Wanderson Lira <wandersonlira.dev1@gmail.com>
2024-07-25 12:00:55 -0300

    :sparkles: feat: Resolved #12 - Especificado parâmetros de CRUD

--- src/main/java/com/symplesweb/controller/resource/ParticipanteEventoResource.java
+++ src/main/java/com/symplesweb/controller/resource/ParticipanteEventoResource.java
@@ -27,51 +27,79 @@ import com.symplesweb.model.entities.Evento;
 import com.symplesweb.model.entities.Participante;
 import com.symplesweb.model.entities.ParticipanteEvento;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-@RequestMapping(value = "/participantes-eventos")
+@RequestMapping(value = "/participantes-eventos", produces = "application/json")
+@Tag(name = "Participante-Evento", description = "Neste endpoint acontece requisitos funcionais de CRUD relacionado a compra/escolha de eventos.")
 public class ParticipanteEventoResource {
-	
-	
+
 	@Autowired
 	private ParticipanteEventoService service;
 	@Autowired
 	private EventoService eventoService;
 	@Autowired
 	private ParticipanteService participanteService;
-	
-	
-	
+
+	@Operation(summary = "Realiza a junção do participante ao evento", method = "POST", description = "Mediante a compra/escolha do determinado evento pelo participante "
+			+ "é gerado um 'tickecode' identificando cada ingresso.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "O status HTTP \"201 Created\" é utilizado como resposta de sucesso, indica que a "
+					+ "requisição foi bem sucedida e que um novo produtor foi criado. "),
+			@ApiResponse(responseCode = "400", description = "O código de status de resposta HTTP 400 Bad Request indica que o servidor não pode "
+					+ "ou não irá processar a requisição devido a alguma coisa que foi entendida como um erro do cliente (por exemplo, sintaxe de "
+					+ "requisição mal formada, algum campo vazio ou algum campo fora do padrão estabelecido).") })
 	@PostMapping
 	public ResponseEntity<ParticipanteEventoDtoView> save(@RequestBody ParticipanteEventoDto participanteEventoDto) {
-		
+
 		Participante entityParticipante = this.participanteService.findById(participanteEventoDto.getIdParticipante());
 		Evento entityEvento = this.eventoService.findById(participanteEventoDto.getIdEvento());
-		
+
 		ParticipanteEvento novoParticipanteEvento = new ParticipanteEvento(null, entityParticipante, entityEvento);
-		
+
 		novoParticipanteEvento = this.service.save(novoParticipanteEvento);
-		
+
 		return ResponseEntity.status(HttpStatus.CREATED).body(new ParticipanteEventoDtoView(novoParticipanteEvento));
 	}
+
 	
-	
+	@Operation(summary = "Exibe todos os participantes que possui evento comprado/escolhido.", method = "GET", description = "Este endpoint retorna o 'ticketCode' "
+			+ "correspondente ao ingresso, o participante e o evento.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "A solicitação foi bem-sucedida"),
+			@ApiResponse(responseCode = "404", description = "O servidor não pode encontrar o recurso solicitado")
+	})
 	@GetMapping
 	public ResponseEntity<List<ParticipanteEventoDtoView>> findAll() {
-		List<ParticipanteEventoDtoView> participanteEventoDtoView = service.findAll()
-				.stream().map(participanteEvento -> new ParticipanteEventoDtoView(participanteEvento))
+		List<ParticipanteEventoDtoView> participanteEventoDtoView = service.findAll().stream()
+				.map(participanteEvento -> new ParticipanteEventoDtoView(participanteEvento))
 				.collect(Collectors.toList());
 		return ResponseEntity.ok().body(participanteEventoDtoView);
 	}
+
 	
-	
+	@Operation(summary = "Baseado no 'id', exibe o participante que possui evento comprado/escolhido.", method = "GET", description = "Este endpoint retorna o 'ticketCode' "
+			+ "correspondente ao ingresso, o participante e seu respectivo evento.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "A solicitação foi bem-sucedida"),
+			@ApiResponse(responseCode = "404", description = "O servidor não pode encontrar o recurso solicitado")
+	})
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<ParticipanteEventoDtoView> findById(@PathVariable Long id) {
 		ParticipanteEvento participanteEvento = service.findById(id);
 		return ResponseEntity.ok().body(new ParticipanteEventoDtoView(participanteEvento));
 	}
+
 	
-	
+	@Operation(summary = "Baseado no 'CPF', exibe o participante que possui evento comprado/escolhido.", method = "GET", description = "Este endpoint retorna o 'ticketCode' "
+			+ "correspondente ao ingresso, o participante e seu respectivo evento.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "A solicitação foi bem-sucedida"),
+			@ApiResponse(responseCode = "404", description = "O servidor não pode encontrar o recurso solicitado")
+	})
 	@GetMapping(value = "/reservas/{cpf}")
 	public ResponseEntity<List<ParticipanteEventoDtoView>> findReservaByCPF(@PathVariable String cpf) {
 		List<ParticipanteEvento> listParticipanteEvento = service.findReservaByCPF(cpf);
@@ -80,35 +108,46 @@ public class ParticipanteEventoResource {
 				.collect(Collectors.toList());
 		return ResponseEntity.ok().body(listParticipanteEventoDtoView);
 	}
+
 	
-	
-	
+	@Operation(summary = "Atualiza para outro 'evento'", method = "PATH", description = "Será realizado uma busca baseado no 'id' "
+			+ "do Participante-Evento, caso o 'id' exista no banco e o 'id' do Evento informado no corpo do método seja diferente do armazenado no servidor, acontecerá a atualização para outro Evento.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "O status HTTP \"201 Created\" é utilizado como resposta de sucesso, indica que a "
+					+ "requisição foi bem sucedida e que um novo recurso foi criado. "),
+			@ApiResponse(responseCode = "400", description = "O código de status de resposta HTTP 400 Bad Request indica que o servidor não pode "
+					+ "ou não irá processar a requisição devido a alguma coisa que foi entendida como um erro do cliente (por exemplo, sintaxe de "
+					+ "requisição mal formada, algum campo vazio ou algum campo fora do padrão estabelecido).")
+	})
 	@PatchMapping
 	public ResponseEntity<ParticipanteEventoDtoView> updateParticipanteEvento(
 			@RequestParam(value = "idParticipanteEvento") Long idParticipanteEvento,
 			@RequestBody ParticipanteEventoUpdateDto participanteEventoUpdateDto) {
-		
+
 		ParticipanteEvento participantEvent = this.service.findById(idParticipanteEvento);
-		
+
 		if (participanteEventoUpdateDto.getIdEvento() != participantEvent.getEvento().getIdEvento()) {
-			Evento entityEvent = this.eventoService.findById(
-					participanteEventoUpdateDto.getIdEvento());
+			Evento entityEvent = this.eventoService.findById(participanteEventoUpdateDto.getIdEvento());
 			participantEvent.setEvento(entityEvent);
 		}
-		
+
 		ParticipanteEvento participantEventToUpdated = this.service.save(participantEvent);
-		
+
 		return ResponseEntity.status(HttpStatus.OK).body(new ParticipanteEventoDtoView(participantEventToUpdated));
 	}
+
 	
-	
-	
+	@Operation(summary = "Exclui o 'ingresso' baseado no 'id'", method = "DELETE", description = "A exclusão acontecerá nas seguintes "
+			+ "condições: 1° - ID deve existir no servidor.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "404", description = "O servidor não pode encontrar o recurso solicitado"),
+			@ApiResponse(responseCode = "422", description = "A solicitação não pôde ser atendida devido a erros semânticos."),
+			@ApiResponse(responseCode = "500", description = "O servidor encontrou uma situação com a qual não sabe lidar.")
+	})
 	@DeleteMapping(value = "/{idParticipanteEvento}")
 	@ResponseStatus(value = HttpStatus.NO_CONTENT)
 	public void deleteParticipanteEvento(@PathVariable Long idParticipanteEvento) {
 		this.service.deleteById(idParticipanteEvento);
 	}
-	
-	
 
 }
